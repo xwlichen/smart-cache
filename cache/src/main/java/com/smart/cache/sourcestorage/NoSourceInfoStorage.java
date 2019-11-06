5648dde801f2a85c4ddcabec1a5b8be3c5081e19
xwlichen <1960003945@qq.com>
2019-11-06 22:20:43 +0800

    commit

---
+++ cache/src/main/java/com/smart/cache/sourcestorage/NoSourceInfoStorage.java
@@ -0,0 +1,25 @@
+package com.smart.cache.sourcestorage;
+
+
+import com.smart.cache.SourceInfo;
+
+/**
+ * {@link SourceInfoStorage} that does nothing.
+ *
+ * @author Alexey Danilov (danikula@gmail.com).
+ */
+public class NoSourceInfoStorage implements SourceInfoStorage {
+
+    @Override
+    public SourceInfo get(String url) {
+        return null;
+    }
+
+    @Override
+    public void put(String url, SourceInfo sourceInfo) {
+    }
+
+    @Override
+    public void release() {
+    }
+}
---
+++ cache/src/main/java/com/smart/cache/sourcestorage/SourceInfoStorage.java
@@ -0,0 +1,18 @@
+package com.smart.cache.sourcestorage;
+
+
+import com.smart.cache.SourceInfo;
+
+/**
+ * Storage for {@link SourceInfo}.
+ *
+ * @author Alexey Danilov (danikula@gmail.com).
+ */
+public interface SourceInfoStorage {
+
+    SourceInfo get(String url);
+
+    void put(String url, SourceInfo sourceInfo);
+
+    void release();
+}
